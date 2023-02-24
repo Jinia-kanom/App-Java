cb2e723ac2c69570eae64499be44d9f7738be78f
Jinia-kanom <zafrinakanom2003@gmail.com>
2023-02-24 16:33:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("We are start the Javascript");
+
+// variable
+
+// key name = value; 5
+var result = 4.94;
+
+// console.log(result);
+
+var name = "Jinia Kanom";
+// console.log(name);
+console.log(name, "got GPA", result, "in SSC Exam");
